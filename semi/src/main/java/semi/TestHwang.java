868e6e779ff8a526d30d0e887e1c49583551cc23
beetrbgus <devbeetrb0214@gmail.com>
2021-10-18 19:59:47 +0900

    어림도 없죠 쿠쿠루삥빵뽕

--- semi/src/main/java/semi/TestHwang.java
+++ semi/src/main/java/semi/TestHwang.java
@@ -1,8 +1,9 @@
 package semi;
 
-public class TestHong {
+public class TestHwang {
 	public static void main(String[] args) {
 		
 		System.out.println("제가 접수하겠습니다.");
+		System.out.println("그럴 수 없죠.");
 	}
 }
