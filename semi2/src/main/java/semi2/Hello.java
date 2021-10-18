52611d3a65773c7421745ea6282dca041d3d64b5
hongbba <88869198+hongbba@users.noreply.github.com>
2021-10-18 17:55:56 +0900

    홍성진 업로드

--- semi2/src/main/java/semi2/Hello.java
+++ semi2/src/main/java/semi2/Hello.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 package semi2;
 
@@ -8,14 +8,6 @@ public class Hello {
 		System.out.println("홍성진");
 
 		System.out.println("황인빈");
-	}
-}
-=======
-package semi2;
 
-public class Hello {
-	public static void main(String[] args) {
-		System.out.println("김종엽");
 	}
 }	
->>>>>>> branch 'main' of https://github.com/hiphop5782/kh8semi.git
