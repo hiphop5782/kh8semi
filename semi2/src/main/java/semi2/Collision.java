d7e6f64b8bb5751327218f6993a82c3141c4363d
rlawhdduq <rlawhdduq95@gmail.com>
2021-10-18 17:54:40 +0900

    김종엽

--- semi2/src/main/java/semi2/Collision.java
+++ semi2/src/main/java/semi2/Collision.java
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-package semi2;
 
-public class Collision {
-	public static void main(String[] args) {
-		System.out.println("김종엽");
-	}
-}
-=======
 package semi2;
 
 public class Collision {
@@ -14,6 +6,7 @@ public class Collision {
 		System.out.println("첫번째 이클립스!");
 		System.out.println("두번째 이클립스!");
 		System.out.println("김동율");
+		System.out.println("김종엽");
 	}
 }
->>>>>>> branch 'main' of https://github.com/hiphop5782/kh8semi
+
